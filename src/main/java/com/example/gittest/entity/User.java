22f6373d1f16bfb78673f3a4c2fb87a8c9f0809b
Khodyko Igor <gaes1k@yandex.ru>
2023-02-17 23:36:47 +0300

    abandone redundat import

--- src/main/java/com/example/gittest/entity/User.java
+++ src/main/java/com/example/gittest/entity/User.java
@@ -4,7 +4,6 @@ package com.example.gittest.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
