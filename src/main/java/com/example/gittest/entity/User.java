8a5df8c279b1171c0e289aa48d7bc5658db698bd
Khodyko Igor <gaes1k@yandex.ru>
2023-02-17 23:35:12 +0300

    usef filled

--- src/main/java/com/example/gittest/entity/User.java
+++ src/main/java/com/example/gittest/entity/User.java
@@ -1,5 +1,21 @@
 package com.example.gittest.entity;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class User {
+    @Id
+    private Long id;
     private String name;
+    private Integer age;
+
 }
